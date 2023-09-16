206aa484b85e26a862bafa3f1803642b1e9ddfd3
Gordon Cheung <gordonhwc1@gmail.com>
2023-09-16 09:28:06 -0700

    newsdata api setup

--- back_end/main.py
+++ back_end/main.py
@@ -1,4 +1,13 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from urllib.parse import quote
+import requests
+import os
+
+load_dotenv()
+
+news_data_api_key = os.getenv("NEWS_DATA_API_KEY")
+news_data_api = "https://newsdata.io/api/1/news?apikey=" + news_data_api_key
 
 if __name__ == "main":
     app = FastAPI()
@@ -6,3 +15,9 @@ if __name__ == "main":
 @app.get("/")
 def get_root():
     return {"Hello" : "World"}
+
+@app.get("/news")
+async def get_news(limit: int = 5, query: str = " "):
+    encoded_query = quote(query)
+    res = requests.get(news_data_api + '&q=' + encoded_query)
+    return res.json()

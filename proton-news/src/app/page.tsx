1d86acb4a62a54221d33f0d5d98cf7deeb1e1ac6
Minh Au <auduongminh2004@gmail.com>
2023-09-16 19:44:08 -0400

    add news and its container

--- proton-news/src/app/page.tsx
+++ proton-news/src/app/page.tsx
@@ -4,13 +4,17 @@ import { TypeWriter } from "@/components/TypeWriter";
 import Navbar from '@/components/Navbar';
 import { Introduction } from '@/components/Introduction';
 import { Links } from '@/components/Links';
+import { NewsContainer } from "@/components/NewsContainer";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Introduction />
-      <Links />
+      <div className='w-full flex justify-center'>
+        <NewsContainer />
+      </div>
+      {/* <Links /> */}
     </>
   )
 }
